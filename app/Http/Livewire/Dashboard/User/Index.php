4415de284b85f2ee7d49a3e2557cb216c4308a20
unknown <szuhri1@gmail.com>
2021-12-03 14:40:28 +0700

    add update user

--- app/Http/Livewire/Dashboard/User/Index.php
+++ app/Http/Livewire/Dashboard/User/Index.php
@@ -5,15 +5,23 @@ namespace App\Http\Livewire\Dashboard\User;
 use App\Models\User;
 use Livewire\Component;
 use Log;
+use Validator;
 
 class Index extends Component
 {
     /**
+     * Undocumented variable
      *
+     * @var array $state
+     */
+    public $state;
+
+    /**
+     * Undocumented variable
      *
      * @var integer
      */
-    public $status = 1;
+    public $userId;
 
     /**
      * Undocumented variable
@@ -46,6 +54,45 @@ class Index extends Component
     /**
      * Undocumented function
      *
+     * @param User $user
+     * @return void
+     */
+    public function showEditUser(User $user)
+    {
+
+        $this->state = $user->toArray();
+        $this->userId = $user->id;
+
+        $this->dispatchBrowserEvent('showEditModal');
+
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function updateUser()
+    {
+        $validData = Validator::make($this->state, [
+            'id' => 'required',
+            'name' => 'required|min:3|max:100',
+            'email' => 'required|email|unique:users,email,' . $this->userId,
+            'type' => 'required',
+            'password' => 'sometimes|min:6',
+            'status' => 'required'
+        ])->validate();
+
+        User::whereId($this->userId)->update($validData);
+
+        $this->dispatchBrowserEvent('hideEditModal');
+
+        return redirect()->route('user.index')->with('updateUser', 'User berhasil diupadate.');
+    }
+
+    /**
+     * Undocumented function
+     *
      * @param integer $userId
      * @return void
      */
@@ -53,7 +100,6 @@ class Index extends Component
     {
 
         $this->idRemoveUser = $userId;
-        $this->status = 2;
         $this->dispatchBrowserEvent('showRemoveModal');
 
     }
--- resources/views/livewire/dashboard/user/index.blade.php
+++ resources/views/livewire/dashboard/user/index.blade.php
@@ -1,4 +1,14 @@
 <div>
+    @if (session()->has('updateUser'))
+        <div class="alert alert-success alert-dismissible show fade col-lg-5 col md-5">
+            <div class="alert-body">
+                <button class="close" data-dismiss="alert">
+                    <span>&times;</span>
+                </button>
+                <i class="far fa-lightbulb"></i> {{ session('updateUser') }}
+            </div>
+        </div>
+    @endif
     @if (session()->has('deleteUser'))
         <div class="alert alert-danger alert-dismissible show fade col-lg-5 col md-5">
             <div class="alert-body">
@@ -29,10 +39,14 @@
                     <td>{{ $user->email }}</td>
                     <td>{{ ucwords(\App\Enums\UserType::fromValue($user->type)->description) }}</td>
                     <td>
-                        <button type="button" href="" class="btn btn-outline-info"><i class="fas fa-user-edit"></i></button>
-                        <button type="button" href="" class="btn btn-outline-danger"
-                            wire:click="showDeleteUser({{$user->id}})"
-                        ><i class="fas fa-trash-alt"></i></button>
+                        <button type="button" class="btn btn-outline-info"
+                            wire:click="showEditUser({{$user->id}})"
+                        ><i class="fas fa-user-edit"></i></button>
+                        @if ($user->id !== 1 && Auth::user()->type == 0 && $user->id !== Auth::user()->id)
+                            <button type="button" class="btn btn-outline-danger"
+                                wire:click="showDeleteUser({{$user->id}})"
+                            ><i class="fas fa-trash-alt"></i></button>
+                        @endif
                     </td>
                 </tr>
                 @endforeach
@@ -81,106 +95,112 @@
     <div class="modal fade" tabindex="-1" role="dialog" id="modalEdit" wire:ignore.self>
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Delete User</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="form-group row">
-                                <label class="col-sm-3 col-form-label">User Name</label>
-                                <div class="col-sm-9">
-                                    <input
-                                        type="text"
-                                        class="form-control @error('name') is-invalid @enderror"
-                                        required
-                                        minlength="3"
-                                        maxlength="100"
-                                        name="name"
-                                        value="{{old('name')}}"
-                                        placeholder="ex: my name"
-                                        wire:model="state.name"
-                                    >
-                                    @error('name')
-                                        <small id="" class="sz-error">
-                                            {{ $message }}
-                                        </small>
-                                    @enderror
+                <form wire:submit.prevent="addUser" autocomplete="off" class="needs-validation" novalidate="">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Delete User</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="form-group row">
+                                    <label class="col-sm-3 col-form-label">User Name</label>
+                                    <div class="col-sm-9">
+                                        <input
+                                            type="text"
+                                            class="form-control @error('name') is-invalid @enderror"
+                                            required
+                                            minlength="3"
+                                            maxlength="100"
+                                            name="name"
+                                            value="{{old('name')}}"
+                                            placeholder="ex: my name"
+                                            wire:model="state.name"
+                                        >
+                                        @error('name')
+                                            <small id="" class="sz-error">
+                                                {{ $message }}
+                                            </small>
+                                        @enderror
 
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-3 col-form-label">User Email</label>
-                                <div class="col-sm-9">
-                                    <input
-                                        type="email"
-                                        class="form-control @error('email') is-invalid @enderror"
-                                        required
-                                        autocomplete="off"
-                                        name="email"
-                                        value="{{old('email')}}"
-                                        placeholder="ex: myname@gmail.com"
-                                        wire:model="state.email"
-                                    >
-                                    @error('email')
-                                        <small id="" class="sz-error">
-                                            {{ $message }}
-                                        </small>
-                                    @enderror
+                                <div class="form-group row">
+                                    <label class="col-sm-3 col-form-label">User Email</label>
+                                    <div class="col-sm-9">
+                                        <input
+                                            type="email"
+                                            class="form-control @error('email') is-invalid @enderror"
+                                            required
+                                            autocomplete="off"
+                                            name="email"
+                                            value="{{old('email')}}"
+                                            placeholder="ex: myname@gmail.com"
+                                            wire:model="state.email"
+                                        >
+                                        @error('email')
+                                            <small id="" class="sz-error">
+                                                {{ $message }}
+                                            </small>
+                                        @enderror
 
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-3 col-form-label">User Status</label>
-                                <div class="col-sm-9">
-                                    <select
-                                        class="form-control @error('type') is-invalid @enderror"
-                                        required
-                                        name="type"
-                                        wire:model="state.type"
-                                    >
-                                        <option value="">. . .</option>
-                                        <option {{ old('type') == "0"? 'selected':''}} value="{{ \App\Enums\UserType::Administrator }}">Administrator</option>
-                                        <option {{ old('type') == "1"? 'selected':''}} value="{{ \App\Enums\UserType::User }}">User</option>
-                                    </select>
-                                    @error('type')
-                                        <small id="" class="sz-error">
-                                            {{ $message }}
-                                        </small>
-                                    @enderror
+                                <div class="form-group row">
+                                    <label class="col-sm-3 col-form-label">User Status</label>
+                                    <div class="col-sm-9">
+                                        <select
+                                            class="form-control @error('type') is-invalid @enderror"
+                                            required
+                                            name="type"
+                                            wire:model="state.type"
+                                        >
+                                            <option value="">. . .</option>
+                                            <option {{ old('type') == "0"? 'selected':''}} value="{{ \App\Enums\UserType::Administrator }}">Administrator</option>
+                                            @if ($userId !== 1)
+                                            <option {{ old('type') == "1"? 'selected':''}} value="{{ \App\Enums\UserType::User }}">User</option>
+                                            @endif
+                                        </select>
+                                        @error('type')
+                                            <small id="" class="sz-error">
+                                                {{ $message }}
+                                            </small>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-3 col-form-label">User Password</label>
-                                <div class="col-sm-9">
-                                    <input
-                                        type="password"
-                                        class="form-control @error('password') is-invalid @enderror"
-                                        required
-                                        autocomplete="new-password"
-                                        name="password"
-                                        value="{{old('password')}}"
-                                        wire:model="state.password"
-                                    >
-                                    @error('password')
-                                        <small id="" class="sz-error">
-                                            {{ $message }}
-                                        </small>
-                                    @enderror
+                                <div class="border-top border-info mb-2">
+                                    <small class="text-info"><i class="far fa-lightbulb"></i> Kosongkan password bila tidak ingin mengubahnya!</small>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-sm-3 col-form-label">User Password</label>
+                                    <div class="col-sm-9">
+                                        <input
+                                            type="password"
+                                            class="form-control @error('password') is-invalid @enderror"
+                                            autocomplete="new-password"
+                                            name="password"
+                                            value="{{old('password')}}"
+                                            wire:model="state.password"
+                                        >
+                                        @error('password')
+                                            <small id="" class="sz-error">
+                                                {{ $message }}
+                                            </small>
+                                        @enderror
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="modal-footer bg-whitesmoke br">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-danger"
-                        wire:click.prevent="deleteUser"
-                    ><i class="fas fa-trash-alt"></i> Delete</button>
-                </div>
+                    <div class="modal-footer bg-whitesmoke br">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-info"
+                            wire:click.prevent="updateUser"
+                        ><i class="fas fa-trash-alt"></i> Update</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
@@ -196,6 +216,14 @@
 
         window.addEventListener('hideModalUser', event => {
             $('#modalCreate').modal('hide');
+        })
+
+        window.addEventListener('showEditModal', event => {
+            $('#modalEdit').modal('show');
+        })
+
+        window.addEventListener('hideEditModal', event => {
+            $('#modalEdit').modal('hide');
         })
 
         window.addEventListener('showRemoveModal', event => {
