c182b91f6db248ad40362e976cd209568e2fd424
unknown <szuhri1@gmail.com>
2022-01-08 09:53:52 +0700

    add edit data

--- app/Http/Livewire/Dashboard/Event/Buwuhan/Index.php
+++ app/Http/Livewire/Dashboard/Event/Buwuhan/Index.php
@@ -3,16 +3,39 @@
 namespace App\Http\Livewire\Dashboard\Event\Buwuhan;
 
 use App\Models\Guest;
+use Auth;
 use Livewire\Component;
+use Validator;
 
 class Index extends Component
 {
+    /**
+     * set
+     *
+     * @var bool
+     */
+    public $status = false;
+
     /**
      * set eentId
      *
      * @var integer
      */
     public $eventId;
+
+    /**
+     * set
+     *
+     * @var int
+     */
+    public $guestId;
+
+    /**
+     * set state
+     *
+     * @var array
+     */
+    public $state = [];
 
     /**
      * listener from other componenet
@@ -24,6 +47,23 @@ class Index extends Component
     ];
 
     /**
+     * set auth user id
+     *
+     * @var int|string|null
+     */
+    public $userId;
+
+    /**
+     * set booted
+     *
+     * @return void
+     */
+    public function booted()
+    {
+        $this->userId = Auth::id();
+    }
+
+    /**
      * Undocumented function
      *
      * @return \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
@@ -31,7 +71,55 @@ class Index extends Component
     public function render()
     {
         return view('livewire.dashboard.event.buwuhan.index', [
-            'guests' => Guest::latest()->paginate(10),
+            'guests' => Guest::whereEventId($this->eventId)->paginate(10),
         ]);
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param Guest $guest
+     * @return void
+     */
+    public function showEditGuest(Guest $guest)
+    {
+        $this->guestId = $guest->id;
+        $this->state = $guest->toArray();
+        $this->status = true;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function hideBuwuhan()
+    {
+        $this->status =false;
+        $this->state = [];
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function updateBuwuhan()
+    {
+        $validData = Validator::make($this->state, [
+            'gender' => 'required',
+            'name' => 'required|string|min:2|max:100',
+            'money' => 'required|min:0',
+            'item' => 'nullable',
+            'district' => 'required',
+        ])->validate();
+
+        $validData['event_id'] = $this->eventId;
+        $validData['user_id'] = $this->userId;
+
+        Guest::whereId($this->guestId)->update($validData);
+
+        $this->hideBuwuhan();
+        session()->flash('success', 'Buwuhan berhasil ditambah');
     }
 }
--- resources/views/livewire/dashboard/event/buwuhan/index.blade.php
+++ resources/views/livewire/dashboard/event/buwuhan/index.blade.php
@@ -1,4 +1,5 @@
 <div>
+    @if ($status == false)
     <div class="table-responsive mt-2">
         <table class="table table-striped" id="sortable-table">
             <thead>
@@ -21,10 +22,10 @@
                     <td>{{ $guest->item }}</td>
                     <td>
                         <button type="button" class="btn btn-outline-info"
-                            wire:click="showEditEvent({{$guest->id}})"
+                            wire:click="showEditGuest({{$guest->id}})"
                         ><i class="fas fa-user-edit"></i></button>
                         <button type="button" class="btn btn-outline-danger"
-                            wire:click="showDeleteEvent({{$guest->id}})"
+                            wire:click="showDeleteGuest({{$guest->id}})"
                         ><i class="fas fa-trash-alt"></i></button>
                     </td>
                 </tr>
@@ -32,4 +33,102 @@
             </tbody>
         </table>
     </div>
+    @else
+        <h6 class="section-title mt-1">Edit Buwuhan</h6>
+        <div class="border-top sz-borderheader2 mb-2"></div>
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Name</label>
+            <div class="col-sm-2">
+                <select id="" class="form-control @error('gender') is-invalid @enderror" required
+                    wire:model="state.gender"
+                >
+                    <option value="">. . .</option>
+                    <option value="bapak">Bapak</option>
+                    <option value="ibu">Ibu</option>
+                </select>
+                <small id="" class="sz-color">
+                    Contoh : bapak/ibu
+                </small>
+                @error('gender')
+                    <small id="" class="sz-error">
+                        <br/>  {{ $message }}
+                    </small>
+                @enderror
+            </div>
+            <div class="col-sm-5">
+                <input type="text" class="form-control @error('name') is-invalid @enderror" required
+                    minlength="2"
+                    maxlength="100"
+                    wire:model="state.name"
+                >
+                <small id="" class="sz-color">
+                    Contoh : ahmad
+                </small>
+                @error('name')
+                    <small id="" class="sz-error">
+                        <br/>  {{ $message }}
+                    </small>
+                @enderror
+
+            </div>
+        </div>
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Uang</label>
+            <div class="col-sm-7">
+                <input type="number" min="0" class="form-control @error('money') is-invalid @enderror" required autocomplete="off"
+                    wire:model="state.money"
+                >
+                <small id="" class="sz-color">
+                    Contoh : 20000
+                </small>
+                @error('money')
+                    <small id="" class="sz-error">
+                        <br/> {{ $message }}
+                    </small>
+                @enderror
+            </div>
+        </div>
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Items (pisahkan dengan koma)</label>
+            <div class="col-sm-7">
+                <input type="text" class="form-control @error('item') is-invalid @enderror" autocomplete="off"
+                    wire:model="state.item"
+                >
+                <small id="" class="sz-color">
+                    Contoh : gula 1 kg, mie 2 bungkus
+                </small>
+                @error('item')
+                    <small id="" class="sz-error">
+                        <br/> {{ $message }}
+                    </small>
+                @enderror
+            </div>
+        </div>
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label">District</label>
+            <div class="col-sm-7">
+                <input type="text" class="form-control @error('district') is-invalid @enderror" required autocomplete="off"
+                    wire:model="state.district"
+                >
+                <small id="" class="sz-color">
+                    Contoh : Ngoro
+                </small>
+                @error('district')
+                    <small id="" class="sz-error">
+                        <br/> {{ $message }}
+                    </small>
+                @enderror
+            </div>
+            <div class="col-sm-3">
+                <button
+                    class="btn btn-primary py-2"
+                    wire:click.privent="updateBuwuhan"
+                ><i class="fas fa-plus-circle"></i></button>
+                <button
+                    class="btn btn-danger py-2"
+                    wire:click.privent="hideBuwuhan"
+                ><i class="far fa-window-close"></i></button>
+            </div>
+        </div>
+    @endif
 </div>
