b02d3fb318ce38713577b7b1a5864f975f5fe8e9
unknown <szuhri1@gmail.com>
2022-02-14 11:47:44 +0700

    perbaikan tampilan edit event

--- resources/views/livewire/dashboard/event/index.blade.php
+++ resources/views/livewire/dashboard/event/index.blade.php
@@ -1,37 +1,38 @@
 <div>
-    <div class="card card-primary mt-2">
-        <div class="card-header">
-            <div class="col-6 col-sm-8 col-md-9 pl-0">
-                <h4 class="text-success"><i class="fas fa-th-list"></i> List Events</h4>
-            </div>
-            <div class="col-6 col-sm-4 col-md-3 pr-0">
-                <input class="form-control border border-success" type="text" minlength="1" maxlength="100" placeholder="Search Event . . ."
-                    wire:model="searchEvent"
-                >
+    @if (session()->has('deleteEvent'))
+        <div class="alert alert-danger alert-dismissible show fade col-lg-5 col md-5">
+            <div class="alert-body">
+                <button class="close" data-dismiss="alert">
+                    <span>&times;</span>
+                </button>
+                <i class="far fa-lightbulb"></i> {{ session('deleteEvent') }}
             </div>
         </div>
-        <div class="card-body pt-0">
-            @if (session()->has('deleteEvent'))
-                <div class="alert alert-danger alert-dismissible show fade col-lg-5 col md-5">
-                    <div class="alert-body">
-                        <button class="close" data-dismiss="alert">
-                            <span>&times;</span>
-                        </button>
-                        <i class="far fa-lightbulb"></i> {{ session('deleteEvent') }}
-                    </div>
+    @endif
+    @if (session()->has('updateEvent'))
+        <div class="alert alert-success alert-dismissible show fade col-lg-5 col md-5">
+            <div class="alert-body">
+                <button class="close" data-dismiss="alert">
+                    <span>&times;</span>
+                </button>
+                <i class="far fa-lightbulb"></i> {{ session('updateEvent') }}
+            </div>
+        </div>
+    @endif
+
+    @if ($status == false)
+        <div class="card card-primary mt-2">
+            <div class="card-header">
+                <div class="col-6 col-sm-8 col-md-9 pl-0">
+                    <h4 class="text-success"><i class="fas fa-th-list"></i> List Events</h4>
                 </div>
-            @endif
-            @if (session()->has('updateEvent'))
-                <div class="alert alert-success alert-dismissible show fade col-lg-5 col md-5">
-                    <div class="alert-body">
-                        <button class="close" data-dismiss="alert">
-                            <span>&times;</span>
-                        </button>
-                        <i class="far fa-lightbulb"></i> {{ session('updateEvent') }}
-                    </div>
+                <div class="col-6 col-sm-4 col-md-3 pr-0">
+                    <input class="form-control border border-success" type="text" minlength="1" maxlength="100" placeholder="Search Event . . ."
+                        wire:model="searchEvent"
+                    >
                 </div>
-            @endif
-            @if ($status == false)
+            </div>
+            <div class="card-body pt-0">
                 <div class="table-responsive">
                     <table class="table table-sm table-bordered table-striped">
                         <thead class="bg-success text-center">
@@ -66,66 +67,75 @@
                         </tbody>
                     </table>
                 </div>
-            @else
-                <h6 class="section-title mt-1">Edit Event</h6>
-                <div class="border-top sz-borderheader2 mb-2"></div>
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Event</label>
-                    <div class="col-sm-9">
-                        <input type="text" class="form-control @error('name') is-invalid @enderror" required
-                            minlength="3"
-                            maxlength="100"
-                            wire:model="state.name"
-                        >
-                        <small id="" class="sz-color">
-                            Contoh : pernikahan a dan b
-                        </small>
-                        @error('name')
-                            <small id="" class="sz-error">
-                                <br/>  {{ $message }}
-                            </small>
-                        @enderror
 
-                    </div>
+                <div class="col-12 col-sm-12 col-md-12 col-lg-12 text-right">
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination justify-content-end">
+                            {{ $events->links() }}
+                        </ul>
+                    </nav>
                 </div>
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Date</label>
-                    <div class="col-sm-4">
-                        <input type="datetime-local" class="form-control @error('date') is-invalid @enderror" required autocomplete="off"
-                            wire:model="state.date"
-                        >
-                        <small id="" class="sz-color">
-                            Contoh : 01-01-2021
-                        </small>
-                        @error('date')
-                            <small id="" class="sz-error">
-                                <br/> {{ $message }}
-                            </small>
-                        @enderror
-                    </div>
-                    <div class="col-sm-4">
-                        <button
-                            class="btn btn-primary py-2"
-                            wire:click.privent="updateEvent"
-                        ><i class="fas fa-plus-circle"></i></button>
-                        <button
-                            class="btn btn-danger py-2"
-                            wire:click.privent="hideEditEvent"
-                        ><i class="far fa-window-close"></i></button>
-                    </div>
-                </div>
-            @endif
 
-            <div class="col-12 col-sm-12 col-md-12 col-lg-12 text-right">
-                <nav aria-label="Page navigation example">
-                    <ul class="pagination justify-content-end">
-                        {{ $events->links() }}
-                    </ul>
-                </nav>
             </div>
-
         </div>
-    </div>
+
+    @else
+        <div class="col-12 col-sm-12 col-lg-8 ml-n3">
+            <div class="card card-primary">
+                <div class="card-body">
+                    <h6 class="text-primary"><i class="far fa-calendar-alt"></i> Edit Event</h6>
+                    <div class="border-top sz-borderheader2 mb-2"></div>
+                    <div class="form-group mb-0">
+                        <label class="col-sm-2 col-form-label">Event</label>
+                        <div class="col-sm-12">
+                            <input type="text" class="form-control @error('name') is-invalid @enderror" required
+                                minlength="3"
+                                maxlength="100"
+                                wire:model="state.name"
+                            >
+                            <small id="" class="sz-color">
+                                Contoh : pernikahan a dan b
+                            </small>
+                            @error('name')
+                                <small id="" class="sz-error">
+                                    <br/>  {{ $message }}
+                                </small>
+                            @enderror
+
+                        </div>
+                    </div>
+                    <div class="form-group mb-0">
+                        <label class="col-sm-2 col-form-label">Date</label>
+                        <div class="input-group">
+                            <div class="col-sm-9">
+                                <input type="datetime-local" class="form-control @error('date') is-invalid @enderror" required autocomplete="off"
+                                    wire:model="state.date"
+                                >
+                                <small id="" class="sz-color">
+                                    Contoh : 01-01-2021
+                                </small>
+                                @error('date')
+                                    <small id="" class="sz-error">
+                                        <br/> {{ $message }}
+                                    </small>
+                                @enderror
+                            </div>
+                            <div class="col-sm-3">
+                                <button
+                                    class="btn btn-success py-2"
+                                    wire:click.privent="updateEvent"
+                                ><i class="far fa-save"></i></button>
+                                <button
+                                    class="btn btn-danger py-2"
+                                    wire:click.privent="hideEditEvent"
+                                ><i class="far fa-window-close"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
 
     {{-- Modal Delete --}}
     <div class="modal fade" tabindex="-1" role="dialog" id="modalDeleteEvent" wire:ignore.self>
